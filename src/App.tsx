0f83d409cb5f1ddce9c97fa9163a07c715319359
Tuuu <tutengfei1994@gmail.com>
2020-05-11 11:00:12 +0800

    增加首页网络请求，测试 nginx 反向代理

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FunctionComponent = () => {
-  const [x, setX] = useState(1);
   useEffect(() => {
-    console.log(x);
-  }, [x]);
+    Axios.get('/mock/44/test')
+      .then(res => {
+        console.log(res.data.data.name);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div
-      className="App"
-      onClick={e => {
-        console.log(e);
-      }}
-    >
+    <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          onClick={e => {
-            console.log(e);
-          }}
-        />
+        <img src={logo} className="App-logo" alt="logo" />
         <Link to="/center" className="App-link">
           前往个人中心
         </Link>
+        {/* <button type="button">点我</button> */}
       </header>
     </div>
   );
